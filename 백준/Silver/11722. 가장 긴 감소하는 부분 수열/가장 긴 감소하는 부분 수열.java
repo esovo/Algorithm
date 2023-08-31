73113ee83ab334e0fc82130db44a68932b4d625d
JoEunseo <81341162+esovo@users.noreply.github.com>
2023-09-01 00:38:29 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 96 ms, Memory: 11900 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -10,19 +10,17 @@ public class Main {
          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
          int N = Integer.parseInt(br.readLine());
-         int[] arr = new int[N+1];
+         int[] arr = new int[N];
 
          StringTokenizer st = new StringTokenizer(br.readLine());
-         for(int i=1; i<N+1; i++) arr[i] = Integer.parseInt(st.nextToken());
+         for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-         int[] dp = new int[N+1];
-         dp[0] = 0;
-         dp[1] = 1;
+         int[] dp = new int[N];
 
          int max = 1;
-         for(int i=2; i<N+1; i++){
+         for(int i=0; i<N; i++){
              dp[i] = 1;
-             for(int j=1; j<i; j++) if(arr[i] < arr[j]) dp[i] = Math.max(dp[i], dp[j]+1);
+             for(int j=0; j<i; j++) if(arr[i] < arr[j]) dp[i] = Math.max(dp[i], dp[j]+1);
              max = Math.max(max, dp[i]);
          }
 
