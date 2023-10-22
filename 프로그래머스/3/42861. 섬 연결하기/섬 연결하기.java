262444541f94ecd363bf50c22ee4921f9cf43a07
esovo <81341162+esovo@users.noreply.github.com>
2023-10-22 20:09:24 +0900

    [level 3] Title: 섬 연결하기, Time: 0.55 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -7,23 +7,22 @@ class Solution {
         Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2]));
 
         p = new int[n];
-        for (int i=0; i<n; i++) p[i] = i;
+        for(int i=0; i<n; i++) p[i] = i;
 
         int answer = 0;
         int count = 0;
-
-        for (int i=0; i<costs.length; i++) {
+        for(int i=0; i<costs.length; i++) {
             int start = costs[i][0];
             int end = costs[i][1];
             int cost = costs[i][2];
 
-            if (find(start) != find(end)) {
+            if(find(start) != find(end)) {
                 union(start, end);
                 answer += cost;
                 count++;
             }
 
-            if (count == n-1) break;
+            if(count == n-1) break;
         }
 
         return answer;
