4d31700d0a281d16278e001b6b4389adaaa7de16
esovo <81341162+esovo@users.noreply.github.com>
2024-01-25 22:10:44 +0900

    [Silver V] Title: 피보나치 수 4, Time: 120 ms, Memory: 18068 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        BigInteger[] dp = new BigInteger[10001];
+        dp[0] = new BigInteger("0");
+        dp[1] = new BigInteger("1");
+        for(int i=2; i<=N; i++){
+            dp[i] = dp[i-1].add(dp[i-2]);
+        }
+        System.out.println(dp[N]);
+    }
+
+}
