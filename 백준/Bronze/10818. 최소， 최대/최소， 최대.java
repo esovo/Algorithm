a1eb365ee4ab086bd1cfc498ad342c9ac7ac6956
esovo <81341162+esovo@users.noreply.github.com>
2024-02-10 15:02:32 +0900

    [Bronze III] Title: 최소, 최대, Time: 1016 ms, Memory: 129496 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        sb.append(arr[0]).append(" ").append(arr[N-1]);
+        System.out.println(sb);
+    }
+
+}
