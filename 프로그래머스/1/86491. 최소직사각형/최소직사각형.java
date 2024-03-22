0f02750a71534941cd437bf94337854cdeaa9895
esovo <81341162+esovo@users.noreply.github.com>
2024-03-22 23:00:30 +0900

    [level 1] Title: 최소직사각형, Time: 1.72 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int maxW = 0, maxH = 0;
+        for(int i=0; i<sizes.length; i++){
+            if(sizes[i][0] > sizes[i][1]){
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+            maxW = Math.max(maxW, sizes[i][0]);
+            maxH = Math.max(maxH, sizes[i][1]);
+        }
+        
+        return maxW*maxH;
+    }
+}
