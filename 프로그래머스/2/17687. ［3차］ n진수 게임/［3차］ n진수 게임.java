718fe4fd45731d22a64fd299fb4e3c83dabb8f6c
esovo <81341162+esovo@users.noreply.github.com>
2024-03-23 03:30:28 +0900

    [level 2] Title: [3차] n진수 게임, Time: 30.58 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    
+    public String solution(int n, int t, int m, int p) {
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<t*m; i++){
+            String[] tmp = Integer.toString(i, n).toUpperCase().split("");
+            for(String s : tmp) list.add(s);
+        }
+        
+        // 튜브가 말해야하는 수 담기
+        StringBuilder ans = new StringBuilder();
+        for(int i=p-1; i<t*m; i+=m) ans.append(list.get(i));
+        return ans.toString();
+    }
+    
+}
